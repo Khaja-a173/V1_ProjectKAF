4afb05c85fd6010954acd415807d1a3189287d17
Khaja-a173 <shaik.khaja.moinuddin173@gmail.com>
2025-08-31 14:56:19 +1000

    Deleted vitest.ui.config.ts

--- server/src/lib/codegen.ts
+++
@@ -1,8 +0,0 @@
-import { customAlphabet } from 'nanoid';
-
-const alphabet = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
-const nano = customAlphabet(alphabet, 4);
-
-export function generateRestaurantCode() { 
-  return nano(); 
-}
--- server/src/types/fastify-aug.d.ts
+++
@@ -1,9 +0,0 @@
-import 'fastify';
-
-declare module 'fastify' {
-  interface FastifyReply {
-    requireAuth: (req: FastifyRequest) => void;
-    requireRole: (req: FastifyRequest, roles: string[]) => void;
-    requireTenantCtx: (req: FastifyRequest, tenantId?: string) => void;
-  }
-}
